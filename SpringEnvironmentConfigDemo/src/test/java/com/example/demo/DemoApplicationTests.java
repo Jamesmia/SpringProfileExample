bada6d1ec79d3791b2a3eba088b463d1f2a49c4a
Jamesmia <james.adms@hotmail.com>
2017-11-13 23:40:46 -0600

    With Test
    With Test

--- SpringEnvironmentConfigDemo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ SpringEnvironmentConfigDemo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	EnvironmentBox envBox;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("Test Environment Box: " + envBox.getName());
 	}
 
 }
